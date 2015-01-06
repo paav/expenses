1212f2e7f8dce83cc979b63ee0b082e1bbfdd2d9
Alexey Panteleiev <paav@inbox.ru>
2015-01-06 08:32:40 +0700

    Add bootstrap navigation.

--- protected/views/layouts/main.php
+++ protected/views/layouts/main.php
@@ -6,50 +6,39 @@
 $baseUrl = Yii::app()->request->baseUrl;
 ?>
 <!DOCTYPE html>
-  <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
-    <link rel="stylesheet" href="<?php echo $baseUrl; ?>/css/menu.css">
-
+<meta charset="utf-8">
+<script src="//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js"></script>
+<script src="<?php echo $baseUrl; ?>/protected/vendor/bootstrap/js/dropdown.js"></script>
+<link href="<?php echo $baseUrl; ?>/protected/vendor/bootstrap-custom/bootstrap-custom.css" rel="stylesheet">
 <link href="<?php echo $baseUrl; ?>/css/main-old.css" rel="stylesheet">
 <link href="<?php echo $baseUrl; ?>/css/main.css" rel="stylesheet">
-    <link rel="stylesheet" href="<?php echo $baseUrl; ?>/js/calendar.css">
-    <link rel="stylesheet" href="http://i.icomoon.io/public/temp/8b4d9e3f75/UntitledProject1/style.css">
-    <link href='http://fonts.googleapis.com/css?family=Alegreya+Sans+SC:400,900|Alegreya+Sans:400,900|Bowlby+One+SC' rel='stylesheet' type='text/css'>
-    <?php Yii::app()->clientScript->registerCoreScript('jquery'); ?>
-    <?php //Yii::app()->clientScript->registerScriptFile(Yii::app()->request->baseUrl.'/js/main.js'); ?>
-    <?php Yii::app()->clientScript->registerScriptFile($baseUrl.'/js/calendar.js'); ?>
-    <title><?php echo $this->pageTitle; ?></title>
-  </head>
-  <body>
-    <div id="container">
-      <div id="menu">
-        <ul id="yw2">
-          <li><a href="#">Показать ▾</a>
-            <ul>
-              <li class="active"><a href="<?php echo $this->createAbsoluteUrl('expense/index'); ?>">Все расходы</a></li>
-            </ul>
-          </li>
-          <li><a href="#">Создать ▾</a>
-            <ul>
-              <li><a class="repair" href="<?php echo $this->createAbsoluteUrl('jobExpense/create'); ?>">Расход на работу</a></li>
-              <li><a class="part" href="<?php echo $this->createAbsoluteUrl('partExpense/create'); ?>">Расход на запчасть</a></li>
-            </ul>
-          </li>
-        </ul> 
-      </div>
-      <!--
-      <div id="header">
-        <h1>
-          Car<img src="<?php echo $baseUrl; ?>/images/buy-24px.png" alt="buy">Expensesi!
-        </h1>
-      </div>
-      !-->
-      <div id="content">
-        <?php echo $content; ?>
-      </div>
-      <div id="footer">
-        <!--&copy; Alexey Panteleiev, 2014<br>i--!>
-      </div>
-    </div>
-  </body>
-</html>
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+<link href="//fonts.googleapis.com/css?family=Alegreya+Sans+SC:400,900|Alegreya+Sans:400,900|Bowlby+One+SC" rel="stylesheet">
+<title><?php echo $this->pageTitle; ?></title>
+<nav class="navbar navbar-default">
+  <ul class="nav navbar-nav">
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+        Показать <span class="caret"></span>
+      </a>
+      <ul class="dropdown-menu" role="menu">
+        <li><a href="<?php echo $this->createAbsoluteUrl('expense/index'); ?>">Все расходы</a></li>
+      </ul>
+    </li>
+    <li class="dropdown">
+      <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
+        Создать <span class="caret"></span>
+      </a>
+      <ul class="dropdown-menu" role="menu">
+        <li><a href="<?php echo $this->createAbsoluteUrl('jobExpense/create'); ?>">Расход на работу</a></li>
+        <li><a href="<?php echo $this->createAbsoluteUrl('partExpense/create'); ?>">Расход на запчасть</a></li>
+      </ul>
+    </li>
+  </ul>
+</nav>
+<main class="container">
+  <?php echo $content; ?>
+</main>
+<footer>
+  Footer goes here
+</footer>
